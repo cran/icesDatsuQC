fa93b8800e0663ec9cfe302a20b13c5ce4b18c13
Colin Millar <colin.millar@ices.dk>
2024-10-25 09:20:10 +0000

    version 1.2.0

--- R/runQcChecks.R
+++ R/runQcChecks.R
@@ -7,10 +7,10 @@
 #' @param datasetverID the dataset ID
 #' @param recordType string name of the record type
 #'
-#' @return The list of Datasets that can be screened in DATSU with the IDs
+#' @return The list of check failures
 #'
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' filename <- system.file("test_files/vms_test.csv", package = "icesDatsuQC")
 #' runQCChecks(filename, 145, "VE")
 #' }
@@ -21,32 +21,60 @@
 runQCChecks <- function(filename, datasetverID, recordType) {
   qc <- getListQCChecks(datasetverID, recordType)
 
-  data <- read.csv(filename, header = FALSE)
-  names(data) <- getDataFieldsDescription(datasetverID, recordType)$fieldcode
-  tablename <- paste0("R", recordType)
-  assign(tablename, data)
-
-  checks <-
-    lapply(
-      1:nrow(qc),
-      function(i) {
-        try(
-          sqldf(
-            paste(
-              "select * from", tablename, "where not ",
-              qc$sqlText[i]
-            )
-          )
-        )
-      }
+  # some formatting and mods to T-SQL
+  qc$sqlExpression <-
+    gsub(
+      "year[(]getdate[(][)][)]", format(Sys.time(), "%Y"),
+      qc$sqlExpression, ignore.case = TRUE
     )
-  names(checks) <- qc$check_Description
+  qc$sqlExpression <-
+    gsub(
+      "len[(]", "length(",
+      qc$sqlExpression, ignore.case = TRUE
+    )
+  # qc$sqlExpression <- gsub("isnull[(]", "ifnull(", qc$sqlExpression)
 
-  fails <- checks[which(sapply(checks, function(x) !inherits(x, "try-error") && nrow(x) > 0))]
+  # prepare the data table
+  data <- read.csv(filename, header = FALSE)
+  colnames <- getDataFieldsDescription(datasetverID, recordType)$fieldcode
+
+  if (length(colnames) != ncol(data)) {
+    stop("The number of columns in the data file does not match the number of fields in the dataset")
+  }
+
+  names(data) <- colnames
+  data$Linenumber <- 1:nrow(data)
+  assign(paste0("R", recordType), data)
+
+  # try and run checks
+  try_sqldf <- function(sql) {
+    try(
+      suppressWarnings(
+        sqldf(glue("select * {sql}"))
+      ),
+      silent = TRUE
+    )
+  }
+
+  checks <- lapply(qc$sqlExpression, try_sqldf)
+
+  # report back
+  errored <- which(sapply(checks, function(x) inherits(x, "try-error")))
+  if (length(errored)) {
+    warning(glue("{length(errored)} checks could not be run locally, you may still get errors on submission."))
+  }
+
+  fails <- which(sapply(checks, function(x) !inherits(x, "try-error") && nrow(x) > 0))
   if (length(fails) > 0) {
-    fails
+    warning(glue("{length(fails)} checks failed"))
+
+    cbind(
+      Linenumber = unlist(lapply(checks[fails], "[[", "Linenumber")),
+      qc[rep(fails, sapply(checks[fails], nrow)), c("check_Description", "errorType")]
+    )
   } else {
     message("all checks possible to run in R passed")
+
     invisible(NULL)
   }
 }
